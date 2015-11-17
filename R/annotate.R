9090547fe2d0322cb015f6639d004d741f751829
Marcel Schilling <marcel.schilling@mdc-berlin.de>
2015-11-17 16:20:58 +0100

    fixed hard-coded assembly for metadata

--- R/annotate.R
+++ R/annotate.R
@@ -18,7 +18,7 @@ gtf2sqlite <- function(assembly = c("hg19", "hg38", "mm10", "rn5", "dm6", "WBcel
   gtf.gr <- keepStandardChromosomes(gtf.gr)
   seqlevels(gtf.gr) <- paste("chr", seqlevels(gtf.gr), sep="")
   seqlevels(gtf.gr)[which(seqlevels(gtf.gr) == "chrMT")] <- "chrM"
-  txdb <- makeTxDbFromGRanges(gtf.gr, drop.stop.codons = FALSE, metadata = data.frame(name="Genome", value="GRCh37"))
+  txdb <- makeTxDbFromGRanges(gtf.gr, drop.stop.codons = FALSE, metadata = data.frame(name="Genome", value=assembly))
   saveDb(txdb, file=db.file)
 
 }
