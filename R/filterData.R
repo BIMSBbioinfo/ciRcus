11c46a65c9960f0683bc6d96cc70de6b540d8eeb
Petar Glazar <petar.glazar@gmail.com>
2016-03-11 16:08:36 +0100

    issue06 fixed, find_circ.py 1.2 output is now supported

--- R/filterData.R
+++ R/filterData.R
@@ -8,6 +8,28 @@
 #' @param sites splice site candidates, as read from find_circ.py output by readCircs
 #' @param n_uniq_thr minimal number of unique reads required for calling a splice site
 #'
+# qualFilter <- function(sites, n_uniq_thr=2) {
+#
+#   sites <- sites[!(grepl("circ", sites$name) & (sites$end - sites$start > 100000)),]
+#   sites <- sites[sites$breakpoints <= 1,]
+#   sites <- sites[sites$anchor_overlap <= 2,]
+#   sites <- sites[sites$edits <= 2,]
+#   sites <- sites[sites$chrom != "chrM",]
+#   sites <- sites[sites$n_uniq >= n_uniq_thr,]
+#   sites <- sites[sites$best_qual_A >= 35 | sites$best_qual_B >= 35,]
+#
+#   return(sites)
+# }
+# ---------------------------------------------------------------------------- #
+#' filter find_circ.py-generated splice candidates
+#'
+#' description
+#'
+#' details
+#'
+#' @param sites splice site candidates, as read from find_circ.py output by readCircs
+#' @param n_uniq_thr minimal number of unique reads required for calling a splice site
+#'
 qualFilter <- function(sites, n_uniq_thr=2) {
 
   sites <- sites[!(grepl("circ", sites$name) & (sites$end - sites$start > 100000)),]
@@ -16,7 +38,9 @@ qualFilter <- function(sites, n_uniq_thr=2) {
   sites <- sites[sites$edits <= 2,]
   sites <- sites[sites$chrom != "chrM",]
   sites <- sites[sites$n_uniq >= n_uniq_thr,]
-  sites <- sites[sites$best_qual_A >= 35 | sites$best_qual_B >= 35,]
+
+  qual.ind = rowSums(sites[,grepl('best_qual',colnames(sites)),with=FALSE] > 35) > 0
+  sites <- sites[qual.ind,]
 
   return(sites)
 }
