93bbe2593cf7c95227ad9cddb7d535e1a22ab5bc
Marcel Schilling <marcel.schilling@mdc-berlin.de>
2017-03-12 10:53:38 +0100

    added static code analyis with lintr to tests

---
+++ tests/testthat/test-lint.R
@@ -0,0 +1,7 @@
+# taken from https://github.com/jimhester/lintr#testthat
+if (requireNamespace("lintr", quietly = TRUE)) {
+  context("lints")
+  test_that("Package Style", {
+    lintr::expect_lint_free()
+  })
+}
